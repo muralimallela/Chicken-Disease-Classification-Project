71bd213c9da41059d96d7ccaad5784e8ce4abd31
Murali Krishna Mallela <mmallela9@gmail.com>
2025-04-06 17:05:34 +0530

    Prepare base model completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from cnnClassification import logger
 from cnnClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,4 +13,15 @@ if __name__ == "__main__":
         print(f"{'>>'*20} {STAGE_NAME} completed {'<<'*20}")
     except Exception as e:
         print(e)
+        raise e
+    
+STAGE_NAME = "Prepare Base Model stage"
+if __name__ == "__main__":
+    try:
+        logger.info(f"{'>>'*20} {STAGE_NAME} started {'<<'*20}")
+        pipeline = PrepareBaseModelTrainingPipeline()
+        pipeline.main()
+        logger.info(f"{'>>'*20} {STAGE_NAME} completed {'<<'*20}")
+    except Exception as e:
+        logger.exception(e)
         raise e
